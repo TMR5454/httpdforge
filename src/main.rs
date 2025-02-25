8ce77153e0dc788c117453b798646c903334af03
Tamura Dai <kirinode0@gmail.com>
2025-02-25 10:36:55 +0900

    Support GET method.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,77 @@
 use std::env;
 //use std::fmt::format;
-use anyhow::Context;
+use anyhow::{Context, Result};
 use env_logger;
 use log;
+use std::fs::File;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
-fn http_server() -> Result<(), anyhow::Error> {
-    let linstener = TcpListener::bind("127.0.0.1:8080")?;
+fn handle_connection(mut stream: std::net::TcpStream) -> Result<()> {
+    let mut buffer = [0; 1024];
+
+    /* read request */
+    let _n = stream.read(&mut buffer)?;
+    let s = String::from_utf8(buffer.to_vec()).context("parse Error")?;
+    log::debug!("\n{}", s);
+
+    /* request parse */
+    match s.split_whitespace().collect::<Vec<&str>>().as_slice() {
+        ["GET", path, more @ ..] => {
+            log::debug!("GET {}", path);
+
+            let path = if *path == "/" {
+                std::env::current_dir().context("current_dir()")?.join("index.html")
+            } else {
+                let fullpath = std::env::current_dir().context("current_dir()")?.to_str().context("to_str()")?.to_string();
+                std::path::PathBuf::from(fullpath + path)
+            };
+
+            log::debug!("path: {:?}", path);
+            log::debug!("more: {:?}", more);
+
+            let file = File::open(path);
+            let mut response = String::with_capacity(32);
+
+            match file {
+                Ok(mut file) => {
+                    let mut body = String::new();
+                    file.read_to_string(&mut body)
+                        .context("fail to read file")?;
+
+                    response = format!("HTTP/1.1 200 OK\r\n\r\n{}", body);
+                }
+
+                Err(e) => {
+                    response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}{}", "404 NOT FOUND. ", e);
+                }
+            }
+
+            let _n = stream.write(response.as_bytes()).context("fail to reply")?;
+            stream.flush().context("fail to flush")?;
+        }
+        ["POST", ..] => {
+            log::debug!("POST");
+        }
+        _ => {
+            log::debug!("other");
+            log::debug!(
+                "{:?}",
+                s.split_whitespace().collect::<Vec<&str>>().as_slice()
+            );
+        }
+    }
+
+    Ok(())
+}
+
+fn http_server() -> Result<()> {
+    let linstener = TcpListener::bind("0.0.0.0:8080")?;
 
     for stream in linstener.incoming() {
-        let mut stream = stream.context("faile to create tcp-stream")?;
-        let mut buffer = [0; 1024];
+        let stream = stream.context("faile to create tcp-stream")?;
 
-        if let Ok(_usize) = stream.read(&mut buffer) {
-            let s = String::from_utf8(buffer.to_vec()).context("parse Error")?;
-            log::debug!("\n{}", s);
-            let _n = stream.write(&buffer).context(format!("fail echo \"{}\"reply", s))?;
-        }
+        handle_connection(stream)?;
     }
     Ok(())
 }
@@ -27,5 +81,5 @@ fn main() {
     env::set_var("RUST_LOG", "trace");
     env_logger::init();
 
-    http_server();
+    let _ret = http_server();
 }
